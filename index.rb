e8505639feaf39290c4af8db490119f50979a3c2
cassidytrinh <cassidytrinh@gmail.com>
2018-06-20 18:37:47 +0000

    Done.

--- index.rb
+++ index.rb
@@ -6,28 +6,40 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+  all = []
+  ingredients.each do |sub_ingredients|
+    for i in 0..2
+      all << sub_ingredients[i]
+    end
+  end
+  order.each do |part|
+    if all.count(part) == 0
+      return false
+    end
+  end
+  return true
 end
+
 
 # example city_info hash
 cities = {
-  :new_york => { 
+  :new_york => {
     :mayor => "Bill DeBlasio",
     :population => 8337000,
     :website => "http://www.nyc.gov"
   },
-  :los_angeles => { 
+  :los_angeles => {
     :mayor => "Eric Garcetti",
     :population => 3884307,
     :website => "http://www.lacity.org"
   },
-  :miami => { 
+  :miami => {
     :mayor => "Tomás Regalado",
     :population => 419777,
     :website => "http://www.miamigov.com"
   },
-  :chicago => { 
+  :chicago => {
     :mayor => "Rahm Emanuel",
     :population => 2695598,
     :website => "http://www.cityofchicago.org/"
@@ -35,5 +47,9 @@ cities = {
 }
 
 def city_populations(cities)
-  # your code here!
+  population = 0
+  cities.each do |city, info|
+    population += info.values[1]
+  end
+  return population
 end
